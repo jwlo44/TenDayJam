b1f364485a115fc55d739f882fd48739cb459fbd
Korbin <kshuffleton@gmail.com>
2021-05-09 15:49:27 -0400

    started writing mouse over script before Rider needed to update a million times

---
+++ Assets/Scripts/MouseOver.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Mathematics
+
+public class MouseOver : MonoBehaviour
+{
+    static float3 GetWorldPosition (Camera camera, float worldDepth, bool useOldsystem = true)
+	{
+		float2 
+	}
+
+}
