9eed545eb1dd61b9ea3395194887492372e8786e
Korbin <kshuffleton@gmail.com>
2021-05-11 21:01:12 -0400

    hiker go up

--- Assets/Scripts/Hiker.cs
+++ Assets/Scripts/Hiker.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class Hiker : MonoBehaviour
 {
     [SerializeField] float _hikeSpeed;
+    [SerializeField] float _maxHeight;
 
     void Start()
     {
@@ -17,6 +18,14 @@ public class Hiker : MonoBehaviour
 
     void FixedUpdate()
     {
-        transform.Translate(Time.deltaTime * _hikeSpeed * Vector3.up);
+        if(ShouldGoUp())
+            transform.Translate(Time.deltaTime * _hikeSpeed * Vector3.up);
+    }
+
+    bool ShouldGoUp()
+    {
+        if (transform.position.y < _maxHeight)
+            return true;
+        return false;
     }
 }
