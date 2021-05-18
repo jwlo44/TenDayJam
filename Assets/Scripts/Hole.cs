3eb6d58d45daace8e005e495314bb62cee7d6acf
Korbin <kshuffleton@gmail.com>
2021-05-17 20:41:14 -0400

    hole go up

---
+++ Assets/Scripts/Hole.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hole : MonoBehaviour
+{
+    [SerializeField] float _delay = 10f;
+    [SerializeField] float _holeSpeed = 10f;
+
+    Vector3 _destination = new Vector3(0, 51, 0);
+    
+    void FixedUpdate()
+    {
+        if (_delay >= 0)
+        {
+            _delay -= PauseTimeManager.deltaTime;
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, _destination, 
+                _holeSpeed * PauseTimeManager.deltaTime);
+        }
+    }
+}
